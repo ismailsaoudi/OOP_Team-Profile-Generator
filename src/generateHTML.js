84277e47d1953f5c2f5b17bd6d520bc91ad5fbc8
ismail saoudi <ismail.saoudi@icloud.com>
2022-07-20 14:46:06 -0400

    added styling to card title and icon

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,13 +1,14 @@
-function generateHTML(cards){
-    return `<!DOCTYPE html>
+function generateHTML(cards) {
+  return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
     </head>
-        <title>Document</title>
+        <title>My Team</title>
     </head>
     <body>
         <div class="h-100 text-center text-white p-3 bg-danger  ">
@@ -24,7 +25,7 @@ function generateHTML(cards){
             </div>
       </body>
       </html>
-            `
-  }
-  
-  module.exports=generateHTML
+            `;
+}
+
+module.exports = generateHTML;
